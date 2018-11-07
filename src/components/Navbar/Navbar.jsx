c41411dd1632070c721e365c4383634a5e637717
Ehsanur Rahman <esnr.rahman@gmail.com>
2018-11-06 22:49:41 -0500

    Add enzyme and add tests
    - Add react-router

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import { Menu } from 'semantic-ui-react';
 
 const Navbar = () => (
   <Menu fluid widths={5}>
-    <Menu.Item name='Home' />
-    <Menu.Item name='About Us' />
-    <Menu.Item name='Shariah Compliance' />
-    <Menu.Item name='Helpful Websites' />
-    <Menu.Item name='Contact Us' />
+    <Menu.Item name="Home" />
+    <Menu.Item name="About Us" />
+    <Menu.Item name="Shariah Compliance" />
+    <Menu.Item name="Helpful Websites" />
+    <Menu.Item name="Contact Us" />
   </Menu>
-)
+);
 
 export default Navbar;
